b6db74fe471753ea258c3eee24930c508fba7b6a
TheRealNock <92985337+TheRealNock@users.noreply.github.com>
2022-03-31 14:18:08 +0200

    :sparkles: Header en style toegevoegd

---
+++ teacher-view/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './../Style/Main.css'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <a className='button'>Test</a>
+    </div>
+  )
+}
+
+export default Home
