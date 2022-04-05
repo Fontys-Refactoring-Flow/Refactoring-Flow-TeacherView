187e980b7ff0a5d3660e424849775c17da37769e
TheRealNock <92985337+TheRealNock@users.noreply.github.com>
2022-04-05 09:07:18 +0200

    :sparkles: classes class toegevoegd

--- teacher-view/src/App.js
+++ teacher-view/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './Pages/Home'
 import Header from './Components/Header'
 import Assignments from './Pages/Assignments'
+import Classes from './Pages/Classes';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/assignments' element={<Assignments/>}/>
+          <Route path='/classes' element={<Classes/>}/>
         </Routes>
       </div>
     </Router>
---
+++ teacher-view/src/Pages/Classes.jsx
@@ -0,0 +1,13 @@
+import { React, useState } from 'react'
+import './../Style/Main.css'
+
+const Classes = () => {
+
+  return (
+    <div className='container'>
+        
+    </div>
+  )
+}
+
+export default Classes
