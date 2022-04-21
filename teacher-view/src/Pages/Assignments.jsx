4985bdadfd1161874e8977c58a11eafbee2b9b26
TheRealNock <92985337+TheRealNock@users.noreply.github.com>
2022-04-21 11:34:34 +0200

    :sparkles: Assingment page update

--- teacher-view/src/Pages/Assignments.jsx
+++ teacher-view/src/Pages/Assignments.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './../Style/Main.css'
+import './../Style/Card.css'
+import AssignmentService from '../Services/AssignmentService';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div className='container'>
-      <p className='title'>Not Home</p>
-    </div>
-  )
+class AssignmentsPage extends Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            challenge: []
+        }
+    }
+
+    componentDidMount() {
+        AssignmentService.getChallenges().then((res) => {
+            console.log(res)
+            this.setState({ challenge: res.data });
+        })
+    }
+
+
+    render() {
+        return (
+            <div className='container'>
+                <Link to='addassignment'className='button' style={{float :'left'}}>Add Assignment</Link>
+                <form class="d-flex" style={{marginTop:'32px',paddingLeft:'16px'}}>
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="button" type="submit">Search</button>
+                </form>
+                <p className='title'style={{textAlign:'left'}}>All Assignments</p>
+                {
+                    this.state.challenge.map(
+                        challenge =>
+                            <tr key={challenge.id}>
+                                <div className='card' style={{ width: '18rem', height: '200px', margin: '10px' }}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{challenge.name}</h5>
+                                        <p className="card-text">{challenge.description}</p>
+                                        <Link to='editassignment' className='button' role="button" id={challenge.id}>Edit Assignment</Link>
+                                    </div>
+                                </div>
+                            </tr>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
-export default Home
+export default AssignmentsPage;
