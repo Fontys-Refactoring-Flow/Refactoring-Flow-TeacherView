b199aef27b3d8264b408724877944f4d3d6b33e7
pepijn3 <pepijnvanloenhout@gmail.com>
2022-11-16 10:36:24 +0100

    add assignment fix. only needs codefile

--- teacher-view/src/Pages/AddAssignment.jsx
+++ teacher-view/src/Pages/AddAssignment.jsx
@@ -15,13 +15,16 @@ class AddAssignment extends Component {
             level: '',
             description: '',
             risks: '',
+            language:''
 
         }
         this.changeNameHandler = this.changeNameHandler.bind(this);
         this.changeRisksHandler = this.changeRisksHandler.bind(this);
         this.changeLevelHandler = this.changeLevelHandler.bind(this);
         this.changeDescriptionHandler = this.changeDescriptionHandler.bind(this);
+        this.changeLanguageHandler = this.changeLanguageHandler.bind(this);
         this.saveAssignment = this.saveAssignment.bind(this);
+
     }
 
     saveAssignment = (e) => {
@@ -36,7 +39,7 @@ class AddAssignment extends Component {
         };
         console.log('assignment =>' + JSON.stringify(assignment));
 
-        AssignmentService.createChallange(assignment).then(res =>{
+        AssignmentService.createAssignment(assignment).then(res =>{
             history.push('/assignments')
             window.location.reload(false)
         });
@@ -59,11 +62,8 @@ class AddAssignment extends Component {
     }
 
     changeRisksHandler=(event) =>{
-        this.setState({language: event.target.value});
+        this.setState({risks: event.target.value});
     }
-
-
-
 
 
     render(){
@@ -71,53 +71,63 @@ class AddAssignment extends Component {
         return (
             <div className='container'>
                 <p className='title'>Add an Assignment</p>
-                <form className="row g-3">
-                    <div className='col-md-3'></div>
-                    <div className="col-md-3">
-                        <label className="form-label">Title</label>
-                        <input type="text" className="form-control" value={this.state.name} onChange={this.changeNameHandler} required/>
+                <form>
+                    <div className="row g-3">
+                        <div className="col-md-4">
+                            <label className="form-label">Refactoring type</label>
+                            <input type="text" className="form-control" value={this.state.name} onChange={this.changeNameHandler} required/>
+                        </div>
+                        <div className="col-md-4">
+                            <label className="form-label">Language</label>
+                            <input type="text" className="form-control" value={this.state.language} onChange={this.changeLanguageHandler} required/>
+                        </div>
+                        <div className="col-md-4">
+                            <label className="form-label">level</label>
+                            <input type="number" className="form-control" value={this.state.level} onChange={this.changeLevelHandler} required/>
+                        </div>
                     </div>
-                    <div className="col-md-3">
-                        <label className="form-label">language</label>
-                        <input type="text" className="form-control" value={this.state.language} onChange={this.changeLanguageHandler} required/>
+
+                    <div className="row g-3">
+                        <div className="col-md-6">
+                            <label className="form-label">Description</label>
+                            <textarea rows='4' className="form-control" value={this.state.description} onChange={this.changeDescriptionHandler} required/>
+                        </div>
+                        <div className="col-md-6">
+                            <label className="form-label">Risks</label>
+                            <textarea rows='4' className="form-control" value={this.state.risks} onChange={this.changeRisksHandler} required/>
+                        </div>
+
                     </div>
-                    <div className='col-md-3'></div>
+
+                    <div className="row g-3">
+                        <div className="col-md-12">
+                            <label className="form-label">Code</label>
+                            <textarea rows='8' className="form-control"  required/>
+                        </div>
+
+                    </div>
+
+                    <div className="row g-3">
+                        <div className='col-md-5'></div>
+                        <div className="col-2">
+                            <button disabled={
+                                !this.state.name ||
+                                !this.state.description ||
+                                !this.state.risks ||
+                                !this.state.level ||
+                                !this.state.language} type="submit" className="button" onClick={this.saveAssignment}>Save</button>
+                        </div>
+                        <div className='col-md-4'></div>
+                        <div className='col-md-1'>
+                            <Link to="/assignments" className='button'>Cancel</Link>
+                        </div>
+
+                    </div>
 
                     {/*<div className="col-md-2">
                         <label className="form-label">Duration in minutes</label>
                         <input type="number" className="form-control" value={} onChange={}/>
                     </div>*/}
-                    <div className='col-md-3'></div>
-                    <div className="col-md-3">
-                        <label className="form-label">Language</label>
-                        <input type="text" className="form-control" value={this.state.language} onChange={this.changeLevelHandler} required/>
-                    </div>
-                    <div className="col-md-3">
-                        <label className="form-label">level</label>
-                        <input type="number" className="form-control" value={this.state.language} onChange={this.changeRisksHandler} required/>
-                    </div>
-                    <div className='col-md-3'></div>
-                    <div className="col-6">
-                        <label className="form-label">Description</label>
-                        <textarea rows='4' className="form-control" value={this.state.description} onChange={this.changeDescriptionHandler} required/>
-                    </div>
-                    <div className="col-6">
-                        <label className="form-label">Risks</label>
-                        <textarea rows='4' className="form-control" value={this.state.risks} onChange={this.changeRisksHandler} required/>
-                    </div>
-                    <div className='col-md-5'></div>
-                    <div className="col-2">
-                        <button disabled={
-                            !this.state.name ||
-                            !this.state.description ||
-                            !this.state.risks ||
-                            !this.state.level ||
-                            !this.state.language} type="submit" className="button" onClick={this.saveAssignment}>Save</button>
-                    </div>
-                    <div className='col-md-4'></div>
-                    <div className='col-md-1'>
-                        <Link to="/assignments" className='button'>Cancel</Link>
-                    </div>
                 </form>
             </div>
         )
