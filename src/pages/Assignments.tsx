2a97b59715030e9feea89b557c073f3efe3d4c4a
IemandDieAbeHeet <abe.vriens@gmail.com>
2022-12-01 12:25:14 +0100

    Fix infinite loop

--- src/pages/Assignments.tsx
+++ src/pages/Assignments.tsx
@@ -12,7 +12,7 @@ const AssignmentsPage = () => {
         assignmentService.getAssignments().then((res) => {
             setAssignments(res.data)
         })
-    })
+    }, [])
 
     return (
         <div className='container'>
