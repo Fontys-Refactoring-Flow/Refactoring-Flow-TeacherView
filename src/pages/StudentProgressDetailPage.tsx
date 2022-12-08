d3eaca0303cfe4dfccc8da8133d21d6bfd9976bd
IemandDieAbeHeet <abe.vriens@gmail.com>
2022-12-08 14:53:06 +0100

    Fix old code from merge conflict

--- src/pages/StudentProgressDetailPage.tsx
+++ src/pages/StudentProgressDetailPage.tsx
@@ -1,50 +1,55 @@
 import React, {useEffect, useState} from 'react'
-import '../style/Main.css'
-import '../style/Card.css'
-import studentService from '../services/student.service';
-import { Link } from 'react-router-dom'
-import sessionHandler from '../SesionHandler';
-import assignmentService from '../services/assignment.service';
+import './../style/Main.css'
+import './../style/Card.css'
+import {Link, useSearchParams} from 'react-router-dom'
+import studentService from "../services/student.service";
+import assignmentService from "../services/assignment.service";
 import {StudentType} from "../types/UserTypes";
 import {AssignmentType} from "../types/AssignmentType";
 
 const StudentProgressDetailPage = () => {
-    const [student, setStudent] = useState<StudentType | null>(null)
-    const [assignments, setAssignments] = useState<Array<AssignmentType> | null>(null)
-
+    const [params] = useSearchParams()
+    const [student, setStudent] = useState<StudentType>();
+    const [assignments, setAssignments] = useState<Array<AssignmentType>>([]);
     useEffect(() => {
-        studentService.getStudentById(parseInt(sessionHandler.getStudentId()!)).then((res) => {
+        const studentId = params.get("id")
+        if(studentId == null) return
+        studentService.getStudentById(parseInt(studentId)).then((res) => {
             setStudent(res.data)
         })
 
-        assignmentService.getAssignmentByStudent(parseInt(sessionHandler.getStudentId()!))
-            .then((res) => {
-                setAssignments(res.data)
-            })
+        assignmentService.getAssignmentByStudent(parseInt(studentId)).then((res) => {
+            console.log(res)
+            setAssignments(res.data)
+        })
     }, [])
 
     return(
         <div className='container'>
             <h1 className='title'>{student?.name}</h1>
-            <p className='text'>Assignments in Progress</p>
+            <p className='text'>Assignments finished</p>
             <table className='table table-hover'>
                 <thead>
-                    <tr>
-                        <th>Assignment</th>
-                        <th>Finished</th>
-                    </tr>
+                <tr>
+                    <th>Assignment</th>
+                    <th>Level</th>
+                    <th>Versions</th>
+                </tr>
                 </thead>
                 <tbody>
                 {
-                    assignments?.map((assignment) =>
-                        <tr key={assignment.id}>
-                            <td>{assignment.refactoringType}</td>
-                            <td>no</td>
-                        </tr>
+                    assignments!.map(
+                        assignment =>
+                            <tr key={assignment.id}>
+                                <td>{assignment.refactoringType}</td>
+                                <td>{assignment.level}</td>
+                                <td><Link className='button' to={'codeview/' + assignment.id + '/' + student?.name}>View</Link></td>
+                            </tr>
                     )
                 }
                 </tbody>
             </table>
+
             <Link className='button' to='/progress'>Return</Link>
         </div>
     );
--- src/pages/StudentProgressPage.tsx
+++ src/pages/StudentProgressPage.tsx
@@ -1,57 +1,76 @@
-import React, {useEffect, useState} from 'react'
-import '../style/Main.css'
-import '../style/Card.css'
-import studentService from '../services/student.service';
-import SessionHandler from '../SesionHandler';
+import React, {FormEvent, useEffect, useState} from 'react'
+import './../style/Main.css'
+import './../style/Card.css'
+import {useNavigate} from 'react-router-dom'
 import {StudentType} from "../types/UserTypes";
+import studentService from "../services/student.service";
 
 const StudentProgressPage = () => {
-    const [students, setStudents] = useState<Array<StudentType> | null>(null)
+    const navigate = useNavigate()
+    const [students, setStudents] = useState<Array<StudentType>>([])
+    const [filteredStudents, setFilteredStudents] = useState<Array<StudentType>>([])
+
+    const handleChange = (e: FormEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        if (e.currentTarget.value.length > 0) {
+            setFilteredStudents(students!.filter(student => student.name.toLowerCase().includes(e.currentTarget.value)));
+        }
+        else{
+            setFilteredStudents(students);
+        }
+
+    };
 
     useEffect(() => {
         studentService.getStudents().then((res) => {
-            setStudents(res.data);
-            SessionHandler.clearStudentId();
+            setStudents(res.data)
+            setFilteredStudents(res.data)
         })
     }, [])
 
-    const grabId = (id: number) =>{
-        console.log(id);
-        SessionHandler.setStudentId(id);
-    }
-
     return(
         <div className='container'>
-            <p className='title'>All Students</p>
-                    <table className='table table-hover'>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Semester</th>
-                                <th>Progress</th>
+            <div className='row'>
+                <div className='col-3'>
+                    <p className='title'>All Students</p>
+                </div>
+                <div className='col-4'>
+                    <input
+                        className="searchbar"
+                        type="text"
+                        placeholder="Search student"
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+            <table className='table table-hover'>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Semester</th>
+                    <th>Progress</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    filteredStudents!.map(
+                        student =>
+                            <tr key={student.id}>
+                                <td>{student.name}</td>
+                                <td>{student.semester}</td>
+                                <td>
+                                    <a className='button'
+                                       onClick={() => navigate('/progressDetail?id=' + student.id)}>Progress</a>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            students?.map((student) =>
-                                <tr key={student.id}>
-                                    <td>{student.name}</td>
-                                    <td>{student.semester}</td>
-                                    <td>
-                                        <a id={student.id.toString()}
-                                           className='button'
-                                           onClick={() => grabId(student.id)}
-                                           href='/progressDetail'>Progress</a>
-                                    </td>
-                                </tr>
-                           )
-                        }
-                        </tbody>
-                    </table>
+                    )
+                }
+                </tbody>
+            </table>
 
 
         </div>
     );
 }
 
-export default StudentProgressPage
+export default StudentProgressPage;
